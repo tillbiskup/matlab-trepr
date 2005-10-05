dc96f40fd6d8a4e5981f75e822b5b9921596e4b7
web8 <web8@ce1621cc-b414-0410-8814-85b212be3a50>
2005-10-05 14:27:23 +0000

    added some comments to the conception discussion first try to document a script file using ROBODoc
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@33 ce1621cc-b414-0410-8814-85b212be3a50

---
+++ m-files/pretrigger_offset.m
@@ -0,0 +1,33 @@
+% Copyright (C) 2005 Till Biskup
+% 
+% This file ist free software.
+% 
+%****f* core_routines/pretrigger_offset.m
+%
+% AUTHOR
+%	Till Biskup <till.biskup@physik.fu-berlin.de>
+% MAINTAINER
+%	Till Biskup <till.biskup@physik.fu-berlin.de>
+% COPYRIGHT
+%	(c) 2005 Till Biskup
+%	This file is free software
+% CREATION DATE
+%	2005/09/28
+% VERSION
+%	$Revision$
+% MODIFICATION DATE
+%	$Date$
+% KEYWORDS
+%	transient EPR, fsc2
+%
+% SYNOPSIS
+%	DATA = pretrigger_offset ( input_data )
+%
+% DESCRIPTION
+%	This function...
+%
+%******
+%
+% Just to test: Some comments that do not pass to ROBODoc
+
+function data = pretrigger_offset ( input_data )
