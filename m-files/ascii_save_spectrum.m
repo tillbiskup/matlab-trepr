4a30e20972c10818519a2b44acaf916312a6aa63
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2006-04-10 16:43:28 +0000

    added file ascii_save_spectrum that saves 1D spectra together with their field axis as ASCII data with leading comment minor bug fixes
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@144 ce1621cc-b414-0410-8814-85b212be3a50

---
+++ m-files/ascii_save_spectrum.m
@@ -0,0 +1,114 @@
+% Copyright (C) 2006 Till Biskup
+% 
+% This file ist free software.
+% 
+%****m* core_routines/ascii_save_spectrum.m
+%
+% AUTHOR
+%	Till Biskup <till.biskup@physik.fu-berlin.de>
+% MAINTAINER
+%	Till Biskup <till.biskup@physik.fu-berlin.de>
+% COPYRIGHT
+%	(c) 2006 Till Biskup
+%	This file is free software
+% CREATION DATE
+%	2006/04/10
+% VERSION
+%	$Revision$
+% MODIFICATION DATE
+%	$Date$
+% KEYWORDS
+%	transient EPR, fsc2, ASCII save data
+%
+% SYNOPSIS
+%	ascii_save_spectrum ( filename, spectrum, field_params, time_params )
+%
+% DESCRIPTION
+%	This function saves an 1D spectrum as ASCII file with leading commentaries
+%
+% SOURCE
+
+function ascii_save_spectrum ( filename, spectrum, field_params, time_params )
+
+	fprintf ( '\nFUNCTION CALL: $RCSfile$\n\t$Revision$, $Date$\n\n' );
+	
+	% check for right number of input and output parameters
+
+	if nargin ~= 4
+  
+		error('\n\tThe function is called with the wrong number (%i) of input arguments.\n\tPlease use "help ascii_save_spectrum" to get help.',nargin);
+			% get error if function is called with other than
+			% four input parameters
+	end
+	
+	
+	% open file given by filename for write access
+	
+	fid=0;				% set file identifier to standard value
+	
+	while fid < 1 		% while file not opened
+	
+		[fid,message] = fopen(filename, 'wt');
+						% try to open file 'filename' and return fid and message
+		if fid == -1		% if there are any errors while trying to open the file
+		
+			error('\n\tFile %s could not be opened for write:\n\t%s', filename, message);
+						% display the message from fopen
+						
+		end				% end "if fid"
+		
+	end					% end while loop
+
+	% write header
+
+	comment_char = '%';
+
+	fprintf(fid, '%s This file has been written by the routine $RCSfile$, $Revision$ from $Date$\n', comment_char);
+	fprintf(fid, '%s\n', comment_char);
+	fprintf(fid, '%s Field parameters were:\n%s\tstart field: %i G\n%s\tstop field:  %i G\n%s\tstep width:   %2.1f G\n', comment_char,comment_char,field_params(1),comment_char,field_params(2),comment_char,field_params(3));
+	fprintf(fid, '%s\n', comment_char);
+	fprintf(fid, '%s Time parameters were:\n%s\tnumber of points: %i\n%s\ttrigger position: %i\n%s\tlength:           %i microseconds\n', comment_char,comment_char,time_params(1),comment_char,time_params(2),comment_char,time_params(3));
+	fprintf(fid, '%s\n', comment_char);
+	
+
+	% write data
+	
+	field_boundaries = [ field_params(1) field_params(2) ];
+	field = [ min(field_boundaries) : abs(field_params(3)) : max(field_boundaries) ];
+	
+	j = 1;
+	
+	while (j<=length(spectrum))
+	
+		fprintf(fid, '%5.1f\t%14.12f\n',field(j),spectrum(j));
+		j = j + 1;
+		
+	end;
+	
+	% save close file
+
+	while fid > 2
+		% the fids 0, 1 and 2 are reserved for special purposes (input, output, error)
+		% that's why every other file identifier (file handle) has a number > 2
+	
+		% try to close the file
+		status = fclose(fid);
+
+		% test whether the closing was successful
+		if status == -1
+			% if there are any errors while trying to close the file
+		
+			error('\n\tFile %s could not be closed:\n\t%s', filename, status);
+						% display the message from fclose
+						
+		elseif status == 0
+		
+			% set file identifier to exit while loop
+			fid = -1;
+						
+		end				% end "if fid"
+		
+	end					% end while loop
+	
+
+%*******
