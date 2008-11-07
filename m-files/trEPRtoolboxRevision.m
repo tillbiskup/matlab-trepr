07f091554d1c14fa237d23f926ea40207b076c04
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2008-11-07 13:13:44 +0000

    updated files for version 0.1.4
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@764 ce1621cc-b414-0410-8814-85b212be3a50

--- m-files/trEPRtoolboxRevision.m
+++ m-files/trEPRtoolboxRevision.m
@@ -84,8 +84,8 @@ function [ varargout ] = trEPRtoolboxRevision
 	% If you have questions, call the trEPRinfo routine at the command prompt and
 	% contact the maintainer via the email address given there.
 	
-	trEPRtoolboxRevisionNumber = '0.1.3';
-	trEPRtoolboxRevisionDate = '2008-10-21';
+	trEPRtoolboxRevisionNumber = '0.1.4';
+	trEPRtoolboxRevisionDate = '2008-11-07';
 	
 	if (nargout == 1)
 		% in case the function is called with one output parameter
