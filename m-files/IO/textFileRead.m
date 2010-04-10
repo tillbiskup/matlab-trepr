4572fe2a89a214fe898af791bbfcfb6b448de3e8
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2010-04-10 15:10:10 +0000

    Added comment and input argument parsing to textFileRead
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@1193 ce1621cc-b414-0410-8814-85b212be3a50

--- m-files/IO/textFileRead.m
+++ m-files/IO/textFileRead.m
@@ -1,16 +1,26 @@
 function text = textFileRead(filename)
+% Read text file and save all lines to the output argument. The line ends
+% will be conserved (use of fgets internally).
 
-    text = '';
-    fid = fopen(filename);
-    if fid < 0
-        return
-    end
+% Parse input arguments using the inputParser functionality
+parser = inputParser;   % Create an instance of the inputParser class.
+parser.FunctionName = mfilename; % Function name included in error messages
+parser.KeepUnmatched = true; % Enable errors on unmatched arguments
+parser.StructExpand = true; % Enable passing arguments in a structure
+parser.addRequired('filename', @ischar);
+parser.parse(filename);
+
+text = '';
+fid = fopen(filename);
+if fid < 0
+    return
+end
     
-    while 1
-        tline = fgets(fid);
-        if ~ischar(tline)
-            break
-        end
-        text = sprintf('%s%s',text,tline);
+while 1
+    tline = fgets(fid);
+    if ~ischar(tline)
+        break
     end
-    fclose(fid);
+    text = sprintf('%s%s',text,tline);
+end
+fclose(fid);
