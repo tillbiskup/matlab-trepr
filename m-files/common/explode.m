4e0500555a6db1dfd380ff4d4c7f721e4462f0a4
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2010-05-11 12:07:33 +0000

    fixed install function, added PHP-like function explode
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@1248 ce1621cc-b414-0410-8814-85b212be3a50

---
+++ m-files/common/explode.m
@@ -0,0 +1,11 @@
+function strings = explode(str,separator)
+
+separators = strfind(str,separator);
+strings = cell(length(separators)+1,1);
+strings{1} = str(1:separators(1)-1);
+for k=2:length(separators)
+    strings{k}=str(separators(k-1)+1:separators(k)-1);
+end
+strings{end} = str(separators(length(separators))+1:end);
+
+end
