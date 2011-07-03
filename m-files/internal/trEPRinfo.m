6912a8c824b84b191f40f54f5dcbabc3a8b49b44
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2011-07-03 10:13:34 +0000

    removed long header
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@1317 ce1621cc-b414-0410-8814-85b212be3a50

--- m-files/internal/trEPRinfo.m
+++ m-files/internal/trEPRinfo.m
@@ -1,51 +1,10 @@
-% Copyright (C) 2007 Till Biskup
-%
-% This file ist free software.
-%
-%****f* global_scripts/trEPRinfo.m
-%
-% AUTHOR
-%	Till Biskup <till.biskup@physik.fu-berlin.de>
-% MAINTAINER
-%	Till Biskup <till.biskup@physik.fu-berlin.de>
-% COPYRIGHT
-%	(c) 2007 Till Biskup
-%	This file is free software
-% CREATION DATE
-%	2007/01/31
-% VERSION
-%	$Revision: 1307 $
-% MODIFICATION DATE
-%	$Date: 2011-06-22 06:48:31 +0100 (Mi, 22 Jun 2011) $
-% KEYWORDS
-%	trEPR toolbox, trEPR
-%
-% SYNOPSIS
-%	trEPRinfo
-%
-% DESCRIPTION
-%	This short program prints some basic information according the trEPR toolbox
-%
-% COMPATIBILITY
-%	Tested to be fully compatible with GNU Octave - at least with Mac OS X
-%	on 2007-06-27
-%
-% INPUT PARAMETERS
-%	There are no input parameters at the moment.
-%
-% OUTPUT PARAMETERS
-%	There are no input parameters at the moment.
-%
-% EXAMPLE
-%	To get some basic information according the trEPR toolbox, simply typein
-%
-%		trEPRinfo
-%
-% SOURCE
-
 function trEPRinfo
+%TREPRINFO Display basic information about the trEPR toolbox
 
-	fprintf('\nFUNCTION CALL: $Id: trEPRinfo.m 1307 2011-06-22 05:48:31Z till $\n\n');
+%   Copyright 2007,2011 Till Biskup
+%   $Revision: 1307 $  $Date: 2011-06-22 06:48:31 +0100 (Mi, 22 Jun 2011) $
+
+% 	fprintf('\nFUNCTION CALL: $Id: trEPRinfo.m 1307 2011-06-22 05:48:31Z till $\n\n');
 
 	% check for the right number of input and output parameters
 
@@ -64,13 +23,13 @@ function trEPRinfo
 
 	% ...and here the 'real' stuff goes in
 	
-	[ trEPRtoolboxRevisionNumber, trEPRtoolboxRevisionDate ] = trEPRtoolboxRevision;
+	[ tbRevNo, tbRevDate ] = trEPRtoolboxRevision;
 	DisplayPlatform = platform;
 
 	fprintf('=================================================================\n\n');
 	fprintf(' trEPR toolbox\n');
     fprintf(' a Matlab toolbox for transient Electron Paramagnetic Resonance  \n\n');
-	fprintf('   Release:          %s (%s) \n', trEPRtoolboxRevisionNumber, trEPRtoolboxRevisionDate);
+	fprintf('   Release:          %s (%s) \n', tbRevNo, tbRevDate);
 	fprintf('   Directory:        %s \n',trEPRtoolboxdir);
 	fprintf('   Matlab version:   %s \n',version);
 	fprintf('   Platform:         %s \n\n',DisplayPlatform);
@@ -79,5 +38,5 @@ function trEPRinfo
 	fprintf('   http://till-biskup.de/de/software/matlab/trepr/\n\n');
 	fprintf(' or write an email to <till@till-biskup.de>\n\n');
 	fprintf('=================================================================\n\n');
-
-%******
+    
+end
