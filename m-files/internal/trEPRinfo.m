86ceffd5b28c173dd25f520f06b2672640fbcead
till <till@ce1621cc-b414-0410-8814-85b212be3a50>
2011-07-03 10:23:38 +0000

    streamlined code
    git-svn-id: https://r3c.de/svn/FU/trEPR-Auswertung@1318 ce1621cc-b414-0410-8814-85b212be3a50

--- m-files/internal/trEPRinfo.m
+++ m-files/internal/trEPRinfo.m
@@ -6,37 +6,24 @@ function trEPRinfo
 
 % 	fprintf('\nFUNCTION CALL: $Id: trEPRinfo.m 1307 2011-06-22 05:48:31Z till $\n\n');
 
-	% check for the right number of input and output parameters
-
-	if ( nargin ~= 0 )
-
-		error('\n\tThe function is called with the wrong number (%i) of input arguments.\n\tPlease use "help trEPRinfo" to get help.',nargin);
-
-	end
-
-	if ( nargout ~= 0 )
-
-		error('\n\tThe function is called with the wrong number (%i) of output arguments.\n\tPlease use "help trEPRinfo" to get help.',nargout);
-
-	end
-
-
-	% ...and here the 'real' stuff goes in
-	
-	[ tbRevNo, tbRevDate ] = trEPRtoolboxRevision;
-	DisplayPlatform = platform;
-
-	fprintf('=================================================================\n\n');
-	fprintf(' trEPR toolbox\n');
-    fprintf(' a Matlab toolbox for transient Electron Paramagnetic Resonance  \n\n');
-	fprintf('   Release:          %s (%s) \n', tbRevNo, tbRevDate);
-	fprintf('   Directory:        %s \n',trEPRtoolboxdir);
-	fprintf('   Matlab version:   %s \n',version);
-	fprintf('   Platform:         %s \n\n',DisplayPlatform);
-	fprintf('=================================================================\n\n');
-	fprintf(' For latest information, please visit:\n\n');
-	fprintf('   http://till-biskup.de/de/software/matlab/trepr/\n\n');
-	fprintf(' or write an email to <till@till-biskup.de>\n\n');
-	fprintf('=================================================================\n\n');
+try
+    [ tbRevNo, tbRevDate ] = trEPRtoolboxRevision;
+    DisplayPlatform = platform;
     
+    fprintf('=================================================================\n\n');
+    fprintf(' trEPR toolbox\n');
+    fprintf(' a Matlab toolbox for transient Electron Paramagnetic Resonance  \n\n');
+    fprintf('   Release:          %s (%s) \n', tbRevNo, tbRevDate);
+    fprintf('   Directory:        %s \n',trEPRtoolboxdir);
+    fprintf('   Matlab version:   %s \n',version);
+    fprintf('   Platform:         %s \n\n',DisplayPlatform);
+    fprintf('=================================================================\n\n');
+    fprintf(' For latest information, please visit:\n\n');
+    fprintf('   http://till-biskup.de/de/software/matlab/trepr/\n\n');
+    fprintf(' or write an email to <till@till-biskup.de>\n\n');
+    fprintf('=================================================================\n\n');
+catch exception
+    disp('There seems to be a problem with displaying the info...');
+    throw(exception);
+end
 end
