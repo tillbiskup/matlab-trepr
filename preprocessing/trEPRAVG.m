dfdbd5be99073d34e01c4000883f616f5179ee53
Till Biskup <till@till-biskup.de>
2011-11-18 08:01:28 +0000

    Added preliminary version of AVG function

---
+++ preprocessing/trEPRAVG.m
@@ -0,0 +1,85 @@
+function [avgData] = trEPRAVG(data,parameters)
+% TREPRAVG Average over a range of points in dataset with the parameters
+% provided in parameters.
+%
+% data       - struct 
+%              dataset to perform the averaging for
+% parameters - struct
+%              parameter structure as collected by the trEPRgui_AVGwindow
+%
+%              dimension - string
+%                          'x' or 'y'
+%              start     - scalar
+%                          position in axis to start the averaging at
+%              stop      - scalar
+%                          position in axis to end the averaging at
+%
+% avgData    - structure
+%              contains both the averaged data (in avgData.data)
+%              and all usual parameters of a dataset and the parameters
+%              from the averaging in the history.parameters field
+
+% (c) 2011, Till Biskup
+% 2011-11-18
+
+% Parse input arguments using the inputParser functionality
+p = inputParser;   % Create an instance of the inputParser class.
+p.FunctionName = mfilename; % Function name to be included in error messages
+p.KeepUnmatched = true; % Enable errors on unmatched arguments
+p.StructExpand = true; % Enable passing arguments in a structure
+
+p.addRequired('data', @(x)isstruct(x));
+p.addRequired('parameters', @(x)isstruct(x));
+p.parse(data,parameters);
+
+try
+    % As we inherit most of the fields from the original dataset, copy
+    % original dataset completely in avgData
+    avgData = data;
+    
+    % Perform averaging...
+    switch parameters.dimension
+        case 'x'
+            avgData.data = ...
+                mean(avgData.data(:,parameters.start:parameters.stop),2);
+        case 'y'
+            avgData.data = ...
+                mean(avgData.data(parameters.start:parameters.stop,:),1);
+        otherwise
+            fprintf('\nUnknown dimension to average over: %s\n',...
+                parameters.dimension);
+            % As we do not overwrite the dataset, the original dataset will
+            % be returned
+            return;
+    end
+    
+    % TODO: Handle axes properly
+    
+    % Write history
+    history = struct();
+    history.date = datestr(now,31);
+    history.method = mfilename;
+    % Get username of current user
+    % In worst case, username is an empty string. So nothing should really
+    % rely on it.
+    % Windows style
+    history.system.username = getenv('UserName');
+    % Unix style
+    if isempty(history.system.username)
+        history.system.username = getenv('USER');
+    end
+    history.system.platform = platform;
+    history.system.matlab = version;
+    history.system.trEPR = trEPRinfo('version');
+    
+    % Fiddle around with parameters structure, as it gets to hold all
+    % information about the accumulated datasets as well
+    history.parameters = parameters;
+    
+    avgData.history{end+1} = history;
+    
+catch exception
+    throw(exception);
+end
+
+end
