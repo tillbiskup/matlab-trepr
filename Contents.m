dc9834339c6e7c1df35d29d9fdd608a685f135df
Till Biskup <till@till-biskup.de>
2012-04-23 08:17:04 +0100

    v.0.3.8

--- Contents.m
+++ Contents.m
@@ -1,2 +1,2 @@
 % trEPR Toolbox
-% Version 0.3.8 22-Apr-2012
+% Version 0.3.8 23-Apr-2012
---
+++ internal/trEPRinstall.m
@@ -0,0 +1,143 @@
+function status = trEPRinstall()
+% TREPRINSTALL Installing the trEPR Toolbox on your system
+%
+% Usage
+%   trEPRinstall
+%   status = trEPRinstall
+%
+%   status    - string
+%               Empty if everything went fine, otherwise contains message.
+%
+
+% (c) 2012, Till Biskup
+% 2012-04-23
+
+% There are several tasks a good installation program should perform:
+%
+% 1. Adding the toolbox paths to the Matlab(tm) search path
+%
+% 2. Checking the configuration files and update to new parameters
+
+% First of all, check whether there is already a toolbox installed
+if exist('trEPRgui','file')
+    installed = true;
+else
+    installed = false;
+end
+
+fprintf('\n');
+fprintf('==================================================================\n');
+fprintf('\n');
+if installed
+    fprintf(' WELCOME. This will update\n');
+else
+    fprintf(' WELCOME. This will install\n');
+end
+fprintf('\n');
+fprintf('     trEPR toolbox\n');
+fprintf('     - a Matlab(tm) toolbox for transient EPR spectra  \n');
+fprintf('\n');
+fprintf(' on your system.\n');
+fprintf('\n');
+fprintf('==================================================================\n');
+fprintf('\n');
+if installed
+    userQuestion = 'Do you want to update the toolbox now? Y/n [Y]: ';
+else
+    userQuestion = 'Do you want to install the toolbox now? Y/n [Y]: ';
+end
+reply = input(userQuestion, 's');
+if isempty(reply)
+    reply = 'Y';
+end
+if ~strcmpi(reply,'y')
+    fprintf('\nInstallation aborted... good-bye.\n\n');
+    return;
+end
+
+%-------------------------------------------------------------------------
+% ADDING TOOLBOX PATHS
+paths = getToolboxPaths(trEPRinfo('dir'));
+
+% Print paths that get added
+if installed
+    fprintf('\nRefreshing the following paths in the Matlab(tm) search path:\n\n');
+else
+    fprintf('\nAdding the following paths to the Matlab(tm) search path:\n\n');
+end
+cellfun(@(x)fprintf('  %s\n',char(x)),paths);
+
+% Actually add
+cellfun(@(x)addpath(char(x)),paths);
+
+% Trying to save path
+fprintf('\nSaving path... ')
+spstatus = savepath;
+if spstatus
+    fprintf('FAILED!\n');
+    % Test whether global pathdef.m is writable
+    pathdefFileName = fullfile(matlabroot,'toolbox','local','pathdef.m');
+    fh = fopen(pathdefFileName,'w');
+    if fh == -1
+        fprintf('\n   You have no write permissions to the file\n   %s.\n',...
+            pathdefFileName);
+        fprintf('   Therefore, you need to manually save the Matlab path.\n');
+    else
+        close(fh);
+    end 
+else
+    fprintf('done.\n');
+end
+
+%-------------------------------------------------------------------------
+% UPDATING CONFIGURATION - only if installed before
+if installed
+    fprintf('\nUpdating configuration... ');
+    confFiles = trEPRconf('files');
+    if isempty(confFiles)
+        fprintf('done.\n');
+    else
+        fprintf('\n\n')
+        for k=1:length(confFiles)
+            tocopy = trEPRiniFileRead(confFiles{k},'typeConversion',true);
+            master = trEPRiniFileRead([confFiles{k} '.dist'],...
+                'typeConversion',true);
+            newConf = structcopy(master,tocopy);
+            header = 'Configuration file for trEPR toolbox';
+            trEPRiniFileWrite(confFiles{k},...
+                newConf,'header',header,'overwrite',true);
+            [~,cfname,cfext] = fileparts(confFiles{k});
+            fprintf('  merging %s%s\n',cfname,cfext);
+        end
+        fprintf('\ndone.\n');
+    end
+end
+
+fprintf('\nCongratulations! The trEPR Toolbox has been ')
+if installed
+    fprintf('updated ')
+else
+    fprintf('installed ')
+end
+fprintf('on your system.\n\n');
+fprintf('Please, find below a bit of information about the current install.\n\n')
+
+trEPRinfo;
+fprintf('\n');
+
+end
+
+function directories = getToolboxPaths(path)
+directories = cell(0);
+traverse(path);
+    function traverse(directory)
+        list = dir(directory);
+        for k=1:length(list)
+            if list(k).isdir && ~strncmp(list(k).name,'.',1) && ...
+                    ~strcmp(list(k).name,'private')
+                directories{end+1} = fullfile(directory,list(k).name); %#ok<AGROW>
+                traverse(fullfile(directory,list(k).name));
+            end
+        end
+    end
+end
