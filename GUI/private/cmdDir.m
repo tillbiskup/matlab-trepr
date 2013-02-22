779fb32ab1e93315f6aead0304f5b06915d6a2d5
Till Biskup <till@till-biskup.de>
2013-02-22 19:00:19 +0100

    added dir command for command line; updated help files

---
+++ GUI/private/cmdDir.m
@@ -0,0 +1,120 @@
+function [status,warnings] = cmdDir(handle,opt,varargin)
+% CMDDIR Command line command of the trEPR GUI.
+%
+% Usage:
+%   cmdDir(handle,opt)
+%   [status,warnings] = cmdDir(handle,opt)
+%
+%   handle  - handle
+%             Handle of the window the command should be performed for
+%
+%   opt     - cell array
+%             Options of the command
+%
+%   status  - scalar
+%             Return value for the exit status:
+%              0: command successfully performed
+%             -1: GUI window found
+%             -2: missing options
+%             -3: some other problems
+%
+%  warnings - cell array
+%             Contains warnings/error messages if any, otherwise empty
+
+% (c) 2013, Till Biskup
+% 2013-02-22
+
+status = 0;
+warnings = cell(0);
+
+% Parse input arguments using the inputParser functionality
+p = inputParser;   % Create an instance of the inputParser class.
+p.FunctionName = mfilename; % Function name to be included in error messages
+p.KeepUnmatched = true; % Enable errors on unmatched arguments
+p.StructExpand = true; % Enable passing arguments in a structure
+
+p.addRequired('handle', @(x)ishandle(x));
+p.addRequired('opt', @(x)iscell(x));
+%p.addOptional('opt',cell(0),@(x)iscell(x));
+p.parse(handle,opt,varargin{:});
+handle = p.Results.handle;
+opt = p.Results.opt;
+
+% Get command name from mfilename
+cmd = mfilename;
+cmd = cmd(4:end);
+
+% Is there the GUI requested?
+if (isempty(handle))
+    warnings{end+1} = 'No GUI window could be found.';
+    status = -1;
+    return;
+end
+
+if isempty(opt)
+    trEPRgui_setdirectorieswindow();
+    return;
+end
+
+% Get appdata from handle
+ad = getappdata(handle);
+% Get handles from handle
+% gh = guidata(handle);
+
+switch lower(opt{1})
+    case {'load','loaddir'}
+        if size(opt) < 2
+            trEPRmsg(['Load dir: ' ad.control.dirs.lastLoad],'info');
+            return;
+        else
+            if strcmpi(opt{2},'pwd')
+                ad.control.dirs.lastLoad = pwd;
+            end
+            if exist(opt{2},'dir')
+                ad.control.dirs.lastLoad = opt{2};
+            end
+        end
+    case {'save','savedir'}
+        if size(opt) < 2
+            trEPRmsg(['Load dir: ' ad.control.dirs.lastSave],'info');
+            return;
+        else
+            if strcmpi(opt{2},'pwd')
+                ad.control.dirs.lastSave = pwd;
+            end
+            if exist(opt{2},'dir')
+                ad.control.dirs.lastSave = opt{2};
+            end
+        end
+    case {'savefig','savefigdir','figsave','figsavedir','fig','figdir'}
+        if size(opt) < 2
+            trEPRmsg(['Load dir: ' ad.control.dirs.lastFigSave],'info');
+            return;
+        else
+            if strcmpi(opt{2},'pwd')
+                ad.control.dirs.lastFigSave = pwd;
+            end
+            if exist(opt{2},'dir')
+                ad.control.dirs.lastFigSave = opt{2};
+            end
+        end
+    case {'export','exportdir'}
+        if size(opt) < 2
+            trEPRmsg(['Load dir: ' ad.control.dirs.lastExport],'info');
+            return;
+        else
+            if strcmpi(opt{2},'pwd')
+                ad.control.dirs.lastExport = pwd;
+            end
+            if exist(opt{2},'dir')
+                ad.control.dirs.lastExport = opt{2};
+            end
+        end
+    otherwise
+        return;
+end
+
+setappdata(handle,'control',ad.control);
+
+end
+
