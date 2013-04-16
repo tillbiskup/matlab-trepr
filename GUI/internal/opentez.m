1e87befb58bd5d36b0a2ae24946153e5b410d0cf
Till Biskup <till@till-biskup.de>
2013-04-16 16:36:49 +0200

    Added helper function for lazy people opening tez files by double-clicking them in the Matlab(tm) file browser

---
+++ GUI/internal/opentez.m
@@ -0,0 +1,13 @@
+function opentez(filename)
+% OPENTEZ Helper function allowing to open a dataset in toolbox format by
+% double-clicking the file name in the MATLAB(TM) file browser
+%
+% Usage
+%   opentez(filename)
+
+% (c) 2013, Till Biskup
+% 2013-04-16
+
+trEPRopen(filename);
+
+end
