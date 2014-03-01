5ca7ad51663d0b0c21dbfbe89cb37a25712e113e
Till Biskup <till@till-biskup.de>
2014-03-01 09:46:29 +0100

    Added plotStacked - function to plot stacked 1D traces

---
+++ figure/plotStacked.m
@@ -0,0 +1,85 @@
+function plotStacked(x,y,varargin)
+% PLOTSTACKED Plot several 1D data traces (e.g., slices of 2D data) on top
+% of each other, trying to "compress" the traces as much as possible and
+% using the default Matlab "ColorOrder" and "LineStyleOrder".
+%
+% Usage:
+%   plotStacked(x,y);
+%   plotStacked(x,y,'percentage',<scalar>);
+%
+%   x          - vector
+%                x axis values
+%
+%   y          - matrix
+%                1D data traces
+%
+%   percentage - scalar (OPTIONAL)
+%                Additional spacing between traces as fraction of total
+%                plot height.
+%                Default: 10
+%                
+% Please note that you can pass optional parameters as a structure with the
+% appropriate fields as well. Field names are case insensitive.
+%
+% PLOTSTACKED tries to "compress" the 1D traces as much as possible. If the
+% optional parameter "percentage" is not given, ten percent of the overall
+% "height" of the stacked traces when plotted directly on top of each other
+% is added as an additional spacing between the traces.
+%
+% Current limitation: All traces need to have the same length and share a
+% common x axis (first argument).
+%
+% See also PLOT
+
+% (c) 2014, Till Biskup
+% 2014-03-01
+
+% Parse input arguments using the inputParser functionality
+p = inputParser;            % Create an instance of the inputParser class.
+p.FunctionName = mfilename; % Include function name in error messages
+p.KeepUnmatched = true;     % Enable errors on unmatched arguments
+p.StructExpand = true;      % Enable passing arguments in a structure
+% Add required input arguments
+p.addRequired('x', @(arg)isvector(arg) && ~isscalar(arg));
+p.addRequired('y', @(arg)ismatrix(arg) && ~isscalar(arg) ...
+    && any(eq(length(x),size(y))));
+% Add optional parameters with default values
+p.addParamValue('percentage',10,@isscalar);
+% Parse input arguments
+p.parse(x,y,varargin{:});
+
+% Define percentage of total y to be added to delta
+percentage = 10;
+
+% Get number of 1D traces
+dimY = size(y);
+no1Dtraces = dimY(dimY~=length(x));
+
+% Ensure that traces are stored in columns
+if size(y,1) ~= length(x)
+    y = y';
+end
+
+% Ensure that x is a column vector
+if size(x,1) < size(x,2)
+    x = x';
+end
+
+% Get deltas in y direction
+deltas = abs(min(y(:,2:end)-y(:,1:end-1)));
+
+% Calculate a proper difference between each trace
+% Therefore, calculate the overall space used and take a percentage of this
+additionalDelta = (abs(min(y(:,1))) + max(y(:,end)) + sum(deltas)) * ...
+    p.Results.percentage/100;
+
+% Add deltas and additionalDelta to traces
+for delta = 1:length(deltas)
+    y(:,delta+1) = ...
+        y(:,delta+1)+sum(deltas(1:delta)) + delta*additionalDelta;
+end
+
+% Quick and dirty multiplot
+plot(repmat(x,1,no1Dtraces),y)
+
+end
