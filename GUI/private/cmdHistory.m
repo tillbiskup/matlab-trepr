0c77e49532b04a8c390ee09f04ffd13bf83196cd
Till Biskup <till@till-biskup.de>
2014-07-15 08:04:31 +0200

    Added CMD commands history, panel, run; fix with leading whitespace in CMD; load panel further restructured

---
+++ GUI/private/cmdHistory.m
@@ -0,0 +1,56 @@
+function [status,warnings] = cmdHistory(handle,varargin)
+% CMDHISTORY Command line command of the trEPR GUI.
+%
+% Usage:
+%   cmdHistory(handle,opt)
+%   [status,warnings] = cmdHistory(handle,opt)
+%
+%   handle  - handle
+%             Handle of the window the command should be performed for
+%
+%   opt     - cell array
+%             Options of the command
+%
+%   status  - scalar
+%             Return value for the exit status:
+%              0: command successfully performed
+%             -1: GUI window found
+%             -2: missing options
+%             -3: Failed loading file(s)
+%             -4: some other problems
+%
+%  warnings - cell array
+%             Contains warnings/error messages if any, otherwise empty
+
+% Copyright (c) 2014, Till Biskup
+% 2014-07-15
+
+status = 0;
+warnings = cell(0);
+
+% Parse input arguments using the inputParser functionality
+p = inputParser;   % Create an instance of the inputParser class.
+p.FunctionName = mfilename; % Function name to be included in error messages
+p.KeepUnmatched = true; % Enable errors on unmatched arguments
+p.StructExpand = true; % Enable passing arguments in a structure
+
+p.addRequired('handle', @(x)ishandle(x));
+p.addOptional('opt',cell(0),@(x)iscell(x));
+p.parse(handle,varargin{:});
+handle = p.Results.handle;
+opt = p.Results.opt;
+
+% Get command name from mfilename
+cmd = mfilename;
+cmd = cmd(4:end);
+
+% Is there the GUI requested?
+if (isempty(handle))
+    warnings{end+1} = 'No GUI window could be found.';
+    status = -1;
+    return;
+end
+
+trEPRgui_cmd_historywindow();
+
+end
