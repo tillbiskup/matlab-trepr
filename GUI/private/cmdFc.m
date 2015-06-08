e4eef0015e79c10d9e4c4b380b648314453db868
Till Biskup <till@till-biskup.de>
2015-06-08 15:49:41 +0200

    New routine for frequency correction: trEPRFC; new GUI CMD: fc

---
+++ GUI/private/cmdFc.m
@@ -0,0 +1,122 @@
+function [status,warnings] = cmdFc(handle,opt,varargin)
+% CMDFC Command line command of the trEPR GUI.
+%
+% Usage:
+%   cmdConvert(handle,opt)
+%   [status,warnings] = cmdConvert(handle,opt)
+%
+%   handle  - handle
+%             Handle of the window the command should be performed for
+%
+%   opt     - cell array
+%             Options of the command
+%
+%   status  - scalar
+%             Return value for the exit status:
+%              0: command successfully performed
+%             -1: GUI window found
+%             -2: missing options
+%             -3: some other problems
+%
+%  warnings - cell array
+%             Contains warnings/error messages if any, otherwise empty
+
+% Copyright (c) 2015, Till Biskup
+% 2015-06-08
+
+status = 0;
+warnings = cell(0);
+
+% Parse input arguments using the inputParser functionality
+p = inputParser;            % Create an inputParser instance
+p.FunctionName = mfilename; % Include function name in error messages
+p.KeepUnmatched = true;     % Enable errors on unmatched arguments
+p.StructExpand = true;      % Enable passing arguments in a structure
+
+p.addRequired('handle', @(x)ishandle(x));
+p.addRequired('opt', @(x)iscell(x));
+%p.addOptional('opt',cell(0),@(x)iscell(x));
+p.parse(handle,opt,varargin{:});
+handle = p.Results.handle;
+opt = p.Results.opt;
+
+% Get command name from mfilename
+cmd = mfilename;
+cmd = lower(cmd(4:end));
+
+% Is there the GUI requested?
+if (isempty(handle))
+    warnings{end+1} = 'No GUI window could be found.';
+    status = -1;
+    return;
+end
+
+% Get appdata from handle
+ad = getappdata(handle);
+
+% For convenience and shorter lines
+active = ad.control.data.active;
+
+% If there is no active dataset, return
+if ~active
+    warnings{end+1} = sprintf('Command "%s": No active dataset.',cmd);
+    status = -3;
+    return;
+end
+
+if isempty(opt)
+    warnings{end+1} = sprintf(...
+        'Command "%s": Not enough options. Use "help %s" to get some help.',cmd,cmd);
+    status = -2;
+    return;
+end
+
+% Check whether first option is numeric (-> dataset no) or char
+% (conversion option)
+if ~isnan(str2double(opt{1})) && mod(str2double(opt{1}),1) == 0
+    dataset = str2double(opt{1});
+    % Remove first option
+    opt(1) = [];
+else
+    dataset = active;
+end
+
+% Check first option for special selection of datasets
+switch lower(opt{1})
+    case 'all'
+        dataset = 1:length(ad.data);
+        opt(1) = [];
+    case {'visible','vis'}
+        dataset = ad.control.data.visible;
+        opt(1) = [];
+    case {'invisible','invis','inv'}
+        dataset = ad.control.data.invisible;
+        opt(1) = [];
+end
+
+% Check again for options
+if isempty(opt)
+    warnings{end+1} = sprintf(...
+        'Command "%s": Not enough options. Use "help %s" to get some help.',cmd,cmd);
+    status = -2;
+    return;
+end
+
+newFreq = str2double(opt{1});
+if isnan(newFreq)
+    warnings{end+1} = sprintf(...
+        'Command "%s": Wrong option. Use "help %s" to get some help.',cmd,cmd);
+    status = -2;
+    return;
+end
+    
+% Perform actual frequency conversion
+for idx = 1:length(dataset)
+    ad.data{dataset(idx)} = ...
+        trEPRFC(ad.data{dataset(idx)},'frequency',newFreq);
+end
+setappdata(handle,'data',ad.data);
+update_mainAxis();
+
+end
+
---
+++ preprocessing/trEPRFC.m
@@ -0,0 +1,96 @@
+function dataset = trEPRFC(dataset,varargin)
+% TREPRFC Correct frequency of dataset, i.e. shift field axis accordingly.
+%
+% Usage:
+%   dataset = trEPRFC(dataset)
+%   dataset = trEPRFC(dataset,<parameter>,<value>)
+%
+%   dataset   - struct
+%               Dataset complying to trEPR toolbox dataset structure
+%
+% You can specify optional parameters as key-value pairs. Valid parameters
+% and their values are:
+%
+%   frequency - scalar
+%               Microwave frequency the dataset shall be corrected to
+%               Default: 9.70 GHz
+%
+%   method    - string
+%               Method to use: linear (constant offset for magnetic field
+%               axis) or nonlinear (conversion of field axis in g axis and
+%               afterwards reconversion in magnetic field axis).
+%               Default: linear
+%               NOTE: Currently only 'linear' is supported
+%
+%
+% NOTE: Currently, the routine computes a DeltaB0 for the DeltaFrequency
+% and shifts the magnetic field axis accordingly. This does not account for
+% any intrinsic nonlinearity of the problem.
+
+% Copyright (c) 2015, Till Biskup
+% 2015-06-08
+
+try
+    % Parse input arguments using the inputParser functionality
+    p = inputParser;            % Create inputParser instance.
+    p.FunctionName = mfilename; % Include function name in error messages
+    p.KeepUnmatched = true;     % Enable errors on unmatched arguments
+    p.StructExpand = true;      % Enable passing arguments in a structure
+    p.addRequired('dataset', @(x)isstruct(x));
+    p.addParamValue('frequency',9.7,@(x)isscalar(x));
+    p.addParamValue('method','linear',@(x)any(strcmpi(x,{'linear'})));
+    p.parse(dataset,varargin{:});
+catch exception
+    disp(['(EE) ' exception.message]);
+    return;
+end
+
+assignParsedVariables(p.Results);
+
+h = 6.62606957e-34;
+muB = 9.27400968e-24;
+ge = 2.00231930436153;
+
+oldFrequency = dataset.parameters.bridge.MWfrequency.value;
+DeltaNu = oldFrequency-frequency;
+DeltaB0 = h*DeltaNu*1e9 / (ge*muB); % in T
+
+% Convert field value according to axis unit
+switch lower(dataset.axes.data(2).unit)
+    case 'mt'
+        DeltaB0 = DeltaB0 * 1e3;
+    case 'g'
+        DeltaB0 = DeltaB0 * 1e4;
+    otherwise
+        disp(['(WW) Unknown field unit "' dataset.axes.data(2).unit '".']);
+        return;
+end
+
+
+% Apply changes to dataset
+dataset.axes.data(2).values = dataset.axes.data(2).values - DeltaB0;
+dataset.parameters.bridge.MWfrequency.value = frequency;
+dataset.parameters.bridge.MWfrequency.values = ...
+    dataset.parameters.bridge.MWfrequency.values - DeltaNu;
+
+% Write history
+history = trEPRdataStructure('history');
+history.method = mfilename;
+
+history.parameters = p.Results;
+history.parameters = rmfield(history.parameters,'dataset');
+
+dataset.history{end+1} = history;
+
+end
+
+
+function assignParsedVariables(structure)
+
+parsedVariables = fieldnames(structure);
+for variable = 1:length(parsedVariables)
+    assignin('caller',parsedVariables{variable},...
+        structure.(parsedVariables{variable}));
+end
+
+end
